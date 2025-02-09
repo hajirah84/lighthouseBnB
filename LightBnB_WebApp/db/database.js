e07e607b84697cfd232b09e3995bac188b2e9165
hajirah84 <mianhajirah@gmail.com>
2025-02-08 23:26:06 -0500

    refactored addproperty function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -191,11 +191,67 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // Destructure the property object
+  const {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  } = property;
+
+  // SQL query to insert the new property
+  const queryString = `
+    INSERT INTO properties (
+      owner_id, title, description, thumbnail_photo_url, cover_photo_url,
+      cost_per_night, street, city, province, post_code, country,
+      parking_spaces, number_of_bathrooms, number_of_bedrooms
+    )
+    VALUES (
+      $1, $2, $3, $4, $5,
+      $6, $7, $8, $9, $10, $11,
+      $12, $13, $14
+    )
+    RETURNING *;
+  `;
+
+  // Array of query parameters
+  const queryParams = [
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night * 100, // Convert to cents
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  ];
+
+  // Execute the query
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => result.rows[0]) // Return the newly added property
+    .catch((err) => {
+      console.error("Error adding property:", err.message);
+      return null;
+    });
 };
+
 
 module.exports = {
   getUserWithEmail,
